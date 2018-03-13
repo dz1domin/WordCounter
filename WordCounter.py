074327e3c452314aab72efc301a3be9ce5f7cd3d
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-03-13 23:47:17 +0100

    Some additions but mostly just playing around

--- WordCounter.py
+++ WordCounter.py
@@ -2,3 +2,14 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Program for parsing files similar to linux word counter')
 parser.parse_args()
+
+wordcount = 0
+
+file = open('loremipsum.txt', 'r')
+for line in file:
+    newstring = line.split()
+    wordcount += len(newstring)
+
+print(wordcount)
+
+file.close()
