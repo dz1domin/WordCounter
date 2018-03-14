da9e203ab8355d162a57961e393300c47ea37aec
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-03-14 16:28:13 +0100

    Additional additions

--- WordCounter.py
+++ WordCounter.py
@@ -1,23 +1,71 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Program for parsing files similar to linux word counter')
-parser.add_argument('file', nargs='?', type=argparse.FileType('r'), help='Name file to parse')
-parser.add_argument('-cl', '--COUNTLINES', help='Option to count lines in a given file')
+parser.add_argument('file', type=argparse.FileType('r'), help='Name file to parse')
+
+parser.add_argument('-cl', '--COUNTLINES', help='Use this option to count all line in a given file',
+                    action='store_true', dest='cl', default=False)
+
+parser.add_argument('-cw', '--COUNTWORDS', help='Use this option to count all words in a given file',
+                    action='store_true', dest='cw', default=False)
+
+parser.add_argument('-cc', '--COUNTCHARS', help='Use this option to count all alphabetic characters in the given file',
+                    action='store_true', dest='cc', default=False)
+
+
+def countlines(isTrue, file):
+    lineCount =0
+    if isTrue:
+        for line in file:
+            lineCount += 1
+        file.seek(0, 0)
+        print(lineCount)
+    else:
+        pass
+
+
+def countwords(isTrue, file):
+    wordCount = 0
+    if isTrue:
+        for line in file:
+            wordCount += len(line.split())
+        file.seek(0, 0)
+        print(wordCount)
+    else:
+        pass
+
+def countchars(isTrue, file):
+    charcount = 0
+    if isTrue:
+        for line in file:
+            for singlechar in line:
+                charcount += int(singlechar.isalpha())
+        file.seek(0, 0)
+        print(charcount)
+    else:
+        pass
+
+
 args = parser.parse_args()
-filename = args.file
 
-wordcount = 0
-linecount = 0
-scannedline = ''
+countlines(args.cl, args.file)
 
-if filename is not None:
-    for line in filename:
-        linecount += 1
-        wordcount += len(line.split())
-else:
-    print('No file given, use -h option to see proper syntax')
+countwords(args.cw, args.file)
 
-
-print('Number of lines:', linecount, " Number of words: ", wordcount)
+countchars(args.cc, args.file)
+#
+# wordcount = 0
+# linecount = 0
+# scannedline = ''
+#
+# if filename is not None:
+#     for line in filename:
+#         linecount += 1
+#         wordcount += len(line.split())
+# else:
+#     print('No file given, use -h option to see proper syntax')
+#
+#
+# print('Number of lines:', linecount, " Number of words: ", wordcount)
 
 
