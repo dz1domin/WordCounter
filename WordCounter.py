1bfd46d4c48fb4f41a1a3d683a0836c4a3a38827
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-03-14 21:25:03 +0100

    getting closer maybe

--- WordCounter.py
+++ WordCounter.py
@@ -1,9 +1,11 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Program for parsing files similar to linux word counter')
 
-parser.add_argument('file', type=argparse.FileType('r'), help='Enter filename including its extension, that you would '
-                                                              'like to use this program on(required argument)')
+parser.add_argument('-f', '--file', type=argparse.FileType('r'), default=sys.stdin, action='store', dest='file',
+                    help='Enter filename including its extension, that you would like to use this program '
+                         'on(default is standard input)')
 
 parser.add_argument('-cl', '--COUNTLINES', help='Use this option to count all line in a given file',
                     action='store_true', dest='cl', default=False)
@@ -15,11 +17,14 @@ parser.add_argument('-cc', '--COUNTCHARS', help='Use this option to count all al
                     action='store_true', dest='cc', default=False)
 
 
-def count(cl, cw, cc, file):
+def count_in_file(cl, cw, cc, file):
     linecount = 0
     wordcount = 0
     charcount = 0
+
     for line in file:
+        if line == 'q':
+            break
         if cl:
             linecount += 1
         if cw:
@@ -36,9 +41,33 @@ def count(cl, cw, cc, file):
         print('Char count: ', charcount)
 
 
+def count_in_stdin(cl, cw, cc, stream):
+    linecount = 0
+    wordcount = 0
+    charcount = 0
+    for line in stream:
+        if cl:
+            linecount += 1
+        if cw:
+            for singlestr in line.split():
+                wordcount += singlestr.isalpha()
+        if cc:
+            for singlechar in line:
+                charcount += singlechar.isalpha()
+    if cl:
+        print('Line count: ', linecount)
+    if cw:
+        print('Word count: ', wordcount)
+    if cc:
+        print('Char count: ', charcount)
+
+
 args = parser.parse_args()
 
-count(args.cl, args.cw, args.cc, args.file)
+if args.file == sys.stdin:
+    count_in_stdin(args.cl, args.cw, args.cc, args.file)
+else:
+    count_in_file(args.cl, args.cw, args.cc, args.file)
 
 
 
