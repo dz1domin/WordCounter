7d5597561c55c04ab06641b083995e1857cd0b9a
dz1domin <36998838+dz1domin@users.noreply.github.com>
2018-03-15 11:20:59 +0100

    getting closer maybe

--- WordCounter.py
+++ WordCounter.py
@@ -3,7 +3,7 @@ import sys
 
 parser = argparse.ArgumentParser(description='Program for parsing files similar to linux word counter')
 
-parser.add_argument('-f', '--file', type=argparse.FileType('r'), default=sys.stdin, action='store', dest='file',
+parser.add_argument('-f', '--file', type=argparse.FileType('r'), default=sys.stdin, action='store',
                     help='Enter filename including its extension, that you would like to use this program '
                          'on(default is standard input)')
 
@@ -17,31 +17,7 @@ parser.add_argument('-cc', '--COUNTCHARS', help='Use this option to count all al
                     action='store_true', dest='cc', default=False)
 
 
-def count_in_file(cl, cw, cc, file):
-    linecount = 0
-    wordcount = 0
-    charcount = 0
-
-    for line in file:
-        if line == 'q':
-            break
-        if cl:
-            linecount += 1
-        if cw:
-            for singlestr in line.split():
-                wordcount += singlestr.isalpha()
-        if cc:
-            for singlechar in line:
-                charcount += singlechar.isalpha()
-    if linecount:
-        print('Line count: ', linecount)
-    if wordcount:
-        print('Word count: ', wordcount)
-    if charcount:
-        print('Char count: ', charcount)
-
-
-def count_in_stdin(cl, cw, cc, stream):
+def count(cl, cw, cc, stream):
     linecount = 0
     wordcount = 0
     charcount = 0
@@ -64,10 +40,7 @@ def count_in_stdin(cl, cw, cc, stream):
 
 args = parser.parse_args()
 
-if args.file == sys.stdin:
-    count_in_stdin(args.cl, args.cw, args.cc, args.file)
-else:
-    count_in_file(args.cl, args.cw, args.cc, args.file)
+count(args.cl, args.cw, args.cc, args.file)
 
 
 
